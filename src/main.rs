261c474603030ebbc36275d8845fa45377fea2da
Aidan Lowson <aidan.lowson@processvision.com>
2024-08-21 11:18:56 +0100

    Rand

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-use ferris_says::say;
 use std::io;
-use std::io::{stdout, BufWriter};
+use rand::Rng;
+
 fn main() {
+    let correct = rand::thread_rng().gen_range(1..=10);
+    println!("correct value: {correct}\n");
+    println!("Hey, guess a number:\n");
+    
+    let mut num = String::new();
+
+    io::stdin()
+        .read_line(&mut num)
+        .expect("Error reading input");
+
+    println!("You guessed: {}", num.trim())
+}
+
+/*
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+fn concat_example() {
     let stdout = stdout();
 
     // Concatting strings
@@ -11,16 +28,25 @@ fn main() {
     let width = message.chars().count();
     let mut writer = BufWriter::new(stdout.lock());
     say(&message, width, &mut writer).unwrap();
+}
+*/
 
+/*
+fn loop_example() {
     // Looping through an arr
     let nums = [1, 2, 3, 4, 5];
     println!("{nums:?}");
-
-    // Hnalding data we're not sure about the value of
-    print!("Enter the users name: ");
-    let mut users_name = String::new();
-
-    io::stdin().read_line(&mut users_name);
-
-    println!("Hello {users_name}, Welcome!")
 }
+*/
+/*
+fn taking_user_input_example() {
+       // Handlding data we're not sure about the value of
+       print!("Enter the a user name: \n");
+
+       let mut user_name = String::new();
+   
+       io::stdin().read_line(&mut user_name).expect("Error reading input");
+   
+       println!("Hello {}, Welcome!", user_name.trim())
+}
+*/
